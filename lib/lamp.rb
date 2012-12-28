e15a3d424114cd15dc54e72d9711ee5aecc5ffe9
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-12-28 13:41:50 -0500

    added configuration

--- lib/lamp.rb
+++ lib/lamp.rb
@@ -1,10 +1,33 @@
 # ~*~ encoding: utf-8 ~*~
 require 'lamp/version'
 require 'lamp/constants'
-require 'lamp/support'
-require 'lamp/lesson'
+require 'active_support/core_ext/hash'
 
-# Lamp a.k.a Genie Worker is responsible for compiling lesson sources.
+require 'lamp/support'
+
+# Lamp a.k.a Genie Worker is responsible for compiling lesson sources. When
+# the Lamp module is first loaded, it will read configuration options from
+# {CONFIG_FILE}.
 module Lamp
   extend Support::Logger
+
+  # @return [Support::Settings] lamp's settings singleton
+  def self.settings
+    Support::Settings
+  end
+
+  # Loads configuration options from +file+.
+  # @param [String] file          path to configuration file
+  # @return [Void]
+  def self.configure!(file=CONFIG_FILE)
+    settings.defaults = DEFAULTS
+    if File.exist? CONFIG_FILE then settings.load! CONFIG_FILE
+    else puts "Unable to find configuration file at #{CONFIG_FILE}"
+    end
+  end
+
+  configure!
+
 end
+
+require 'lamp/lesson'
---
+++ lib/lamp/support/settings.rb
@@ -0,0 +1,54 @@
+# ~*~ encoding: utf-8 ~*~
+
+module Lamp
+
+  module Support
+
+    # Adapted from
+    # http://speakmy.name/2011/05/29/simple-configuration-for-ruby-apps/
+    module Settings
+      extend self
+
+      @_settings, @defaults = {}, {}
+      attr_reader :_settings
+      attr_accessor :defaults
+
+      # This is the main point of entry - we call Settings.load! and provide
+      # a name of the file to read as it's argument. We can also pass in some
+      # options, but at the moment it's being used to allow per-environment
+      # overrides in Rails.
+      # @param [String|Hash] arg            filename or settings hash
+      def load!(*args)
+        case (first = args.shift)
+        when nil then raise ArgumentError.new "wrong number of arguments"
+        when String then load_file! first, args
+        when Hash then load_hash! first, args
+        end
+      end
+
+      private
+
+      def load_file!(filename, options = {})
+        newsets = YAML::load_file(filename).deep_symbolize
+        if options[:env] && newsets[options[:env].to_sym]
+          newsets = newsets[options[:env].to_sym]
+        end
+        @_settings.deep_merge! newsets
+      end
+
+      def load_hash!(hash, options = {})
+        @_settings.deep_merge! hash
+      end
+
+      # @example
+      #   Settings.email # => @_settings[:email]
+      def method_missing(name, *args, &block)
+        @_settings[name.to_sym] || @defaults[name.to_sym] ||
+          fail(NoMethodError, "Unknown configuration root #{name}.", caller)
+      end
+
+    end
+
+  end
+
+end
