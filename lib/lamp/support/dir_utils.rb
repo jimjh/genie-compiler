4c60e68b0106128c24a2429befb736128372e487
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-12-30 15:39:17 -0500

    added specs for dir utils

--- lib/lamp/support/dir_utils.rb
+++ lib/lamp/support/dir_utils.rb
@@ -18,16 +18,23 @@ module Lamp
         FileUtils.mkdir_p dir
       end
 
+      # @param  [Pathname] prefix     base directory
+      # @param  [Pathname] path       path to check
+      # @return [Boolean] true iff +path+ is a descendant  of +prefix+
+      def descends_from?(prefix, path)
+        basepath = prefix.to_s + File::SEPARATOR
+        path.cleanpath.to_s.starts_with? basepath
+      end
+
       # Copies +files+ from +source+ to +destination+. Ensures that the files
       # to be copied are really children of +source+.
       # @param [Pathname] source        source directory
       # @param [Pathname] destination   destination directory
       # @param [Array]    files         array of file names
       def copy_secure(source, destination, files)
-        basepath = source.to_s + File::SEPARATOR
         files.each do |file|
-          path = (source + file).cleanpath
-          if path.to_s.starts_with?(basepath) and path.exist?
+          path = source + file
+          if descends_from? source, path and path.exist?
             FileUtils.cp_r path, destination
           else Lamp.logger.warn { 'Ignoring suspicious path %s' % path }
           end
